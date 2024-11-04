61601e39f58284c2f624b804c7990ea876472fa2
Seth Ely <seel6470@colorado.edu>
2024-11-04 19:00:20 +0000

    adding first two routes, index and test connection

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+DB_URL = "postgresql://lab_10_test_db_user:tQ7u6YUYctCEThZ8765T9Bq38FDzJiI8@dpg-csifge1u0jms73fbcag0-a/lab_10_test_db"
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return "Hello World from Seth Ely (seel6470) in 3308"
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect(DB_URL)
+    conn.close()
+    return "Test DB connection successful!"
